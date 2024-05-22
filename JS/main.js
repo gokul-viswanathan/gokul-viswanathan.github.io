84d10bfa4d5bc35c264ecc063f7fe4b34b090425
Gokul Viswanathan <115330141+gokul-viswanathan@users.noreply.github.com>
2024-05-22 17:32:42 -0400

    Update after long time

--- JS/main.js
+++ JS/main.js
@@ -15,3 +15,57 @@ hamburger.addEventListener('click', function () {
 		menu.style.display = "block"
 	} 
 });
+
+// navbar scrolling
+
+var didScroll;// on scroll, let the interval function know the user has scrolled
+var lastScrollTop = 0;
+var delta = 8;
+var navbarHeight = $('nav').outerHeight();
+
+$(window).scroll(function(event){
+  didScroll = true;
+});// run hasScrolled() and reset didScroll status
+
+setInterval(function() {
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
+}, 250);
+
+function hasScrolled() {
+  // do stuff here...
+  var st = $(this).scrollTop();
+  if (Math.abs(lastScrollTop - st) <= delta)
+    	return;
+  if (st > lastScrollTop && st > navbarHeight){  // Scroll Down
+  	$('nav').removeClass('navbar').addClass('nav-up');} 
+	else {  // Scroll Up
+  	  	  $('nav').removeClass('nav-up').addClass('navbar');
+	}
+	lastScrollTop = st;
+}
+	
+var UB = document.getElementById("UB")
+var SE = document.getElementById("SE")
+var ASE = document.getElementById("ASE")
+
+function butonClick(val){
+	if (val == "UB"){
+		$(UB).removeClass("UB-hide").addClass("UB-show");
+		$(SE).removeClass("SE-show").addClass("SE-hide");
+		$(ASE).removeClass("ASE-show").addClass("ASE-hide");
+		// console.log("UB")
+	} else if(val == "SE"){
+		$(SE).removeClass("SE-hide").addClass("SE-show");
+		$(UB).removeClass("UB-show").addClass("UB-hide");
+		$(ASE).removeClass("ASE-show").addClass("ASE-hide");
+		// console.log("SE")
+	} else {
+		$(ASE).removeClass("ASE-hide").addClass("ASE-show");
+		$(UB).removeClass("UB-show").addClass("UB-hide");
+		$(SE).removeClass("SE-show").addClass("SE-hide");
+		// console.log("ASE")
+	}
+}
