afa56a3b55dc871e74b029903bbcc58c3dc04897
gokul <gokulviswanathan25@gmail.com>
2025-04-06 18:15:53 -0400

    Version 3.0.0

--- JS/projects.js
+++ JS/projects.js
@@ -1,45 +1,45 @@
 const projectDatas = [
     {
         "projectName": "Thought Ink",
+        "projectContent": "A lightweight, privacy-focused note-taking app with built-in AI assistance. Designed as a learning project, Thought Ink helps you take structured notes (with headings, bullet points, etc.), store them locally in your browser, and ask AI questions based on your content – all without sending your data to any server.",
+        "techStack": ["Typescript", "React.Js", "Node.Js", "Quill.Js", "Git"],
+        "projectLink" : "https://github.com/gokul-viswanathan/note-taker?tab=readme-ov-file",
+        "show": "1"
+    },
+    {
+        "projectName": "LeanFrame: Efficient Video for ML",
+        "projectContent": "Trimmed the fat off video data—built a smart frame filter that sniffs out and drops redundant frames, making machine learning pipelines leaner and faster.",
+        "techStack": ["Python", "OpenCV", "Numpy"],
+        "projectLink" : "https://github.com/gokul-viswanathan",
+        "show": "1"
+    },
+    {
+        "projectName": "Wishlist Price Monitor",
+        "projectContent": "Built a price-tracking browser buddy that watches your wishlist like a hawk—no more site-hopping or falling for flashy recommendations. Just click, check, and chill till the price is right.",
+        "techStack": ["Javascript", "React"],
+        "projectLink" : "https://github.com/gokul-viswanathan",
+        "show": "1"
+    },
+    {
+        "projectName": "Realistic Hand Motion Simulator",
+        "projectContent": "Simulated a human hand with realistic joint motion using MuJoCo—basically gave a robot a hand to wave, grasp, or even sign. It’s a digital playground for training complex hand movements (sign language was next on the list!).",
+        "techStack": ["Python", "Pytorch", "Numpy", "OpenCV", "Pandas"],
+        "projectLink" : "https://github.com/gokul-viswanathan",
+        "show": "1"
+    },
+    {
+        "projectName": "AWS deeprace",
         "projectContent": "Trained agents on OpenAI’s Cartpole and Mountain Car environments leveraging updated DQN and PPO algo- rithms, accomplishing rapid learning with minimal episodes.Demonstrated the A2C (Actor Critic) algorithm’s superior performance in training motion-related models in Gymna- sium’s Walker2D environment, outperforming other algorithms",
         "techStack": ["Python", "Java"],
-        "projectLink" : "https://vrushank0606.github.io/Portfolio/#home",
-        "show": "1"
+        "projectLink" : "https://github.com/gokul-viswanathan",
+        "show": "0"
     },
     {
-        "projectName": "Product Cost plugin",
-        "projectContent": "Trained agents on OpenAI’s Cartpole and Mountain Car environments leveraging updated DQN and PPO algo- rithms, accomplishing rapid learning with minimal episodes.Demonstrated the A2C (Actor Critic) algorithm’s superior performance in training motion-related models in Gymna- sium’s Walker2D environment, outperforming other algorithms",
-        "techStack": ["Python", "Java"],
-        "projectLink" : "https://vrushank0606.github.io/Portfolio/#home",
-        "show": "1"
-    },
-    {
-        "projectName": "Sign language replication",
-        "projectContent": "Trained agents on OpenAI’s Cartpole and Mountain Car environments leveraging updated DQN and PPO algo- rithms, accomplishing rapid learning with minimal episodes.Demonstrated the A2C (Actor Critic) algorithm’s superior performance in training motion-related models in Gymna- sium’s Walker2D environment, outperforming other algorithms",
-        "techStack": ["Python", "Java"],
-        "projectLink" : "https://vrushank0606.github.io/Portfolio/#home",
-        "show": "1"
-    },
-    {
-        "projectName": "Optimizing Reinforcement Learning algorithms",
-        "projectContent": "Trained agents on OpenAI’s Cartpole and Mountain Car environments leveraging updated DQN and PPO algo- rithms, accomplishing rapid learning with minimal episodes.Demonstrated the A2C (Actor Critic) algorithm’s superior performance in training motion-related models in Gymna- sium’s Walker2D environment, outperforming other algorithms",
-        "techStack": ["Python", "Java"],
-        "projectLink" : "https://vrushank0606.github.io/Portfolio/#home",
-        "show": "1"
-    },
-    {
-        "projectName": "Traffic Vehicle image classification ",
-        "projectContent": "Developed and trained a Convolutional Neural Network (CNN) model employing Python and TensorFlow for image classification, achieving high accuracy on the dataset composed of vehicle images.Created a React web page for receiving vehicle images as input and classifying it employing CNN model.",
-        "techStack": ["Python", "Java"],
-        "projectLink" : "https://vrushank0606.github.io/Portfolio/#home",
-        "show": "1"
-    },
-    {
-        "projectName": "AR-Based Human Face Reconstruction and Cloth Draping ",
+        "projectName": "AR-Based Human Face Reconstruction",
         "projectContent": "Constructed a 3D face reconstruction model using the PifuHD, converting 2D images into accurate 3D models with an 80% skin tone match. Executed conversion of facial images into full-body renderings, harnessing DrapeNet to attire models in chosen garments, thereby elevating visual allure. ",
         "techStack": ["Python", "Java"],
-        "projectLink" : "https://vrushank0606.github.io/Portfolio/#home",
-        "show": "1"
+        "projectLink" : "https://github.com/gokul-viswanathan",
+        "show": "0"
     },
     
 ];
